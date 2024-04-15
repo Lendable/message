2666e9fc46aa62e1c8a42e2ca514a719dcde13f0
Ben Challis <ben@lendable.co.uk>
2024-04-15 10:35:21 +0100

    Add event name resolution

---
+++ tests/fixtures/Event/ExampleBarEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\Lendable\Message\Event;
+
+use Lendable\Message\CorrelationId;
+use Lendable\Message\Event\Event;
+use Lendable\Message\MessageId;
+use Lendable\Message\MessageTrait;
+
+final class ExampleBarEvent implements Event
+{
+    use MessageTrait;
+
+    private function __construct(MessageId $id, CorrelationId $correlationId, MessageId $causationId)
+    {
+        $this->id = $id;
+        $this->correlationId = $correlationId;
+        $this->causationId = $causationId;
+    }
+
+    public static function fresh(CorrelationId $correlationId, ?MessageId $causationId = null): self
+    {
+        $messageId = MessageId::generate();
+
+        return new self($messageId, $correlationId, $causationId ?? $messageId);
+    }
+
+    public static function existing(MessageId $id, CorrelationId $correlationId, MessageId $causationId): self
+    {
+        return new self($id, $correlationId, $causationId);
+    }
+}
