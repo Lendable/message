e433b36926e5ad6b5e0acfabe6e446f278faa92e
Ben Challis <ben-challis@users.noreply.github.com>
2024-06-02 15:31:46 +0100

    fix: add `phpstan-assert` for `non-empty-string` to `MessageId` factories for call site inference (#90)

--- src/Uuid.php
+++ src/Uuid.php
@@ -8,11 +8,15 @@ interface Uuid
 {
     /**
      * @throws InvalidUuidString If the string is not a UUID.
+     *
+     * @phpstan-assert non-empty-string $value
      */
     public static function fromBinaryString(string $value): static;
 
     /**
      * @throws InvalidUuidString If the string is not a UUID.
+     *
+     * @phpstan-assert non-empty-string $value
      */
     public static function fromString(string $value): static;
 
