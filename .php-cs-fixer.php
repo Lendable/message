2ff52a3e5592cfd9030b93ca5adecd530446b7dc
Marcin Michalski <evulmastah@gmail.com>
2025-10-23 11:53:11 +0200

    update another cs rule

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,7 +9,7 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS2x0' => true,
-        '@PER-CS2.0:risky' => true,
+        '@PER-CS2x0:risky' => true,
         '@PHP82Migration' => true,
         '@DoctrineAnnotation' => true,
         'align_multiline_comment' => true,
